ff01dd985b8b86f039cfff46088f5463941d3ecc
yota-hada <hadayota33@gmail.com>
2021-03-25 15:33:00 +0900

    :wrench: Add jest.

---
+++ jest.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  roots: ['<rootDir>'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'json', 'jsx'],
+  testPathIgnorePatterns: [
+    '<rootDir>[/\\\\](node_modules|.next)[/\\\\]',
+    '<rootDir>/test/Storyshots.test.ts',
+  ],
+  transformIgnorePatterns: ['[/\\\\]node_modules[/\\\\].+\\.(ts|tsx)$'],
+  transform: {
+    '^.+\\.(js|ts|tsx)$': 'babel-jest',
+  },
+  watchPlugins: [
+    'jest-watch-typeahead/filename',
+    'jest-watch-typeahead/testname',
+  ],
+  moduleNameMapper: {
+    '\\.(styl|css|less|scss)$': '<rootDir>/test/__mocks__/styleMock.js',
+    '\\.(gif|ttf|eot|svg|png)$': '<rootDir>/test/__mocks__/fileMock.js',
+  },
+  moduleDirectories: ['node_modules', 'src'],
+}
---
+++ test/pages/index.test.tsx
@@ -0,0 +1,7 @@
+import '@jest/globals'
+
+describe('jest 動作確認', () => {
+  test('true toBeTruthy', () => {
+    expect(true).toBeTruthy()
+  })
+})
